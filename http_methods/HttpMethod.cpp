2ad9de2fe46fad4c718929f52c0964822c1a0ef9
leopold <leopold.dva@gmail.com>
2017-03-31 04:23:37 +0300

    Redo HttpMethod, refactor classes, impl moved to cpp

---
+++ http_methods/HttpMethod.cpp
@@ -0,0 +1,5 @@
+//
+// Created by operator on 3/31/17.
+//
+
+#include "HttpMethod.h"
---
+++ http_methods/HttpMethod.h
@@ -0,0 +1,15 @@
+//
+// Created by operator on 3/31/17.
+//
+
+#ifndef HTTPCLIENT_HTTPMETHOD_H
+#define HTTPCLIENT_HTTPMETHOD_H
+
+
+class HttpMethod
+{
+
+};
+
+
+#endif //HTTPCLIENT_HTTPMETHOD_H
